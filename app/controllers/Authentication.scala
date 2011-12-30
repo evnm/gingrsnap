bc9c93ecea8f5a2bb7bf99b6f73746ea4471aa86
Evan Meagher <evan.meagher@gmail.com>
2011-12-29 18:42:16 -0800

    Add modified scalasecure code to app directly.
    This is a bad idea, but hopefully a temporary fix.

---
+++ app/controllers/Authentication.scala
@@ -0,0 +1,135 @@
+/*
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  Created on: 16th August 2011
+ */
+package controllers
+
+import play._
+import play.mvc._
+import play.mvc.results.Redirect
+import play.libs._
+
+import models._
+import secure._
+
+/**
+ * Controller for managing authentication.
+ *
+ * @author Aishwarya Singhal
+ */
+object Authentication extends Controller {
+
+  /**
+   * Displays the login page. If a past request for "remember me" has
+   * generated a cookie, the user will be automatically logged in. Note that
+   * this implementation only puts username into the session which acts as
+   * a successful login token to the system.
+   */
+  def login = {
+    val remember = request.cookies.get("rememberme");
+    if (remember != null && remember.value.indexOf("-") > 0) {
+      val sign = remember.value.substring(0, remember.value.indexOf("-"));
+      val username = remember.value.substring(remember.value.indexOf("-") + 1);
+      if (Crypto.sign(username).equals(sign)) {
+        flash.put("remember", true);
+        flash.put("username", username);
+        session.put("username", username);
+        flash.keep("url");
+        redirectToOriginalURL()
+      }
+    }
+    flash.keep("url");
+    views.Login.html.login("Login")
+  }
+
+
+  /**
+   * Authenticates user inputs in params hash against the security realm.
+   */
+  def authenticate: Redirect =
+    authenticate(params.get("username"), PasswordCredential(params.get("password")))
+
+  /**
+   * Authenticates passed user inputs against the security realm.
+   * @see secure.Security
+   */
+  def authenticate[U <: Credential](username: String, credential: U) = {
+    var url = flash.get("url")
+
+    try {
+      Security.authenticate(username, credential)
+
+      // process the remember me request
+      rememberMe(params.get("remember"), username)
+
+      session.put("username", username);
+      flash.keep
+      redirectToOriginalURL()
+    } catch {
+      case e: AuthenticationFailureException =>
+        flash.error("Login failed", "username")
+        url = "/login"
+        flash.keep
+        Redirect(url)
+      case e: Throwable =>
+        flash.error("Unknown error", "username")
+        url = "/login"
+        flash.keep
+        Redirect(url)
+    }
+  }
+
+  /**
+   * Creates a secure cookie that retains the provided username if
+   * "remember me" checkbox has been checked.
+   */
+  private def rememberMe(remember: String, username: String) = {
+    // Remember if needed
+    if (remember != null && remember.toBoolean) {
+      response.setCookie("rememberme", Crypto.sign(username) + "-" + username, "30d");
+    }
+  }
+
+  /**
+   * Logs out the user and redirects to the root URL ("/").
+   */
+  def logout = {
+    val username = session.get("username")
+    session.clear
+    flash.remove("username")
+    response.removeCookie("rememberme")
+    Security onSuccessfulLogout(username)
+    Redirect("/")
+  }
+
+  /**
+   * Shown in case a user is not not authorized to access a requested URL/ action.
+   */
+  def unauthorized = {
+    views.Login.html.unauthorized("Unauthorized Access")
+  }
+
+  /**
+   * Redirects user to the requested URL post login.
+   */
+  private def redirectToOriginalURL() = {
+    val username = session.get("username")
+    Security.onSuccessfulLogin(username)
+    var url = flash.get("url");
+    if (url == null) {
+      url = "/";
+    }
+    Redirect(url);
+  }
+}
---
+++ app/secure/Security.scala
@@ -0,0 +1,187 @@
+/*
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  Created on: 16th August 2011
+ */
+package secure
+
+import java.lang.reflect._
+
+import play._
+import play.utils._
+import twitter4j.auth.AccessToken
+
+/**
+ * A trait to wrap user authentication credentials.
+ */
+sealed trait Credential
+
+case class PasswordCredential(password: String) extends Credential
+case class TwAuthCredential(token: AccessToken) extends Credential
+
+/**
+ * This is the default security realm that should be overridden in a real world application.
+ *
+ * @author Aishwarya Singhal
+ */
+class Security[T] {
+  /**
+   * This method is called during the authentication process. This is where you check if
+   * the user is allowed to log in into the system. This is the actual authentication process
+   * against a third party system (most of the time a DB).
+   *
+   * @param username
+   * @param credential
+   * @return authenticated token
+   * @throws AuthenticationFailureException if the authentication process fails
+   */
+  def authenticate[U <: Credential](username: String, credential: U): T = {
+    throw new AuthenticationFailureException
+  }
+
+  /**
+   * This method checks that a profile is allowed to view this page/methodbased on roles defined in @Authorize.
+   *
+   * @param token AuthenticatedToken
+   * @param role
+   * @return true if you are allowed to execute this controller method.
+   */
+  def authorize(token: T, role: String): Boolean = {
+    false
+  }
+
+  /**
+   * This method is called after a successful authentication.
+   * You need to override this method if you with to perform specific actions (eg. Record the time the user signed in)
+   */
+  def onSuccessfulLogin(username: String) = {
+
+  }
+
+  /**
+   * This method is called after a successful sign off.
+   * You need to override this method if you wish to perform specific actions (eg. Record the time the user signed off)
+   */
+  def onSuccessfulLogout(username: String) = {
+
+  }
+
+  /**
+   * This method is called if an authorization check does not succeed. By default it throws a UnauthorizedAccessException
+   * @param role
+   */
+  def onAuthorizationFailure(role: String) = {
+    throw new UnauthorizedAccessException
+  }
+}
+
+object Security {
+
+  /**
+   * Authenticates a login attempt.
+   * @param username
+   * @param credential
+   * @return Authenticated token
+   */
+  def authenticate[U <: Credential](username: String, credential: U) = {
+    val token = realm.authenticate(username, credential)
+    AuthenticatedTokenHolder.set(username, token)
+  }
+
+  /**
+   * Checks if the logged in user is authorized to perform the requested action.
+   * @param username
+   * @param role
+   * @return
+   */
+  def authorize(username: String, role: String): Boolean = {
+    realm authorize (AuthenticatedTokenHolder.get(username), role)
+  }
+
+  /**
+   * Called upon a successful login.
+   */
+  def onSuccessfulLogin(username: String) = {
+    realm onSuccessfulLogin(username)
+  }
+
+  /**
+   * Called upon a successful logout.
+   * @param username
+   */
+  def onSuccessfulLogout(username: String) = {
+    realm onSuccessfulLogout(username)
+    AuthenticatedTokenHolder.remove(username)
+  }
+
+  /**
+   * Called in case of an authorization failure.
+   *
+   * @param role
+   * @return
+   */
+  def onAuthorizationFailure(role: String) = {
+    realm onAuthorizationFailure role
+  }
+
+  /**
+   * Identifies the security realm to be invoked.
+   *
+   * @return
+   */
+  private def realm[T <: Security[_]]: T = {
+    var security: Class[_ <: Security[_]] = classOf[Security[_]]
+    var classes = Play.classloader.getAssignableClasses(classOf[Security[_]])
+    if (classes.size() != 0) {
+      security = classes.get(0).asInstanceOf[Class[_ <: Security[_]]];
+    }
+
+    security.newInstance.asInstanceOf[T]
+  }
+}
+
+/**
+ * Container for holding the authenticated tokens. It uses Cache in the background.
+ *
+ * @author Aishwarya Singhal
+ */
+object AuthenticatedTokenHolder {
+  import play.cache.Cache
+
+  /**
+   * Remove the token for a given user name.
+   *
+   * @param username
+   */
+  def remove(username: String) = {
+    Cache.delete(username)
+  }
+
+  /**
+   * Save the token for a given user name.
+   *
+   * @param username
+   */
+  def set[T](username: String, token: T) = {
+    Cache.set(username, token)
+  }
+
+  /**
+   * Fetch the token for a given user name.
+   *
+   * @param username
+   */
+  def get[T](username: String): T = {
+    Cache.get(username).getOrElse(null).asInstanceOf[T]
+  }
+}
