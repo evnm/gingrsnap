a142785d6456bf5fa36a1b893060ccccdfece02f
Evan Meagher <evan.meagher@gmail.com>
2011-12-31 15:45:51 -0800

    Add setup and teardown scripts.
    These assume specific db names and locations, but meh.

---
+++ scripts/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# In order to properly export DATABASE_URL into your shell's env, source this
+# script, as opposed to executing it directly: `. ./scripts/setup.sh`.
+
+echo "Setting DATABASE_URL..."
+read -p "db username? " db_username
+read -s -p "db password? " db_pwd
+export DATABASE_URL=postgres://$db_username:$db_pwd@localhost/gingrsnapdb
+
+echo "Starting postgres instance..."
+pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start
+
+echo "Setup complete!"
