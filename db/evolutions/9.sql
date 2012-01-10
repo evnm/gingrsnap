8a466cac61b58fdc7f247ee69aefa541d0c8585b
Evan Meagher <evan.meagher@gmail.com>
2012-01-10 09:39:08 -0800

    Add Make db evolution.

---
+++ db/evolutions/9.sql
@@ -0,0 +1,14 @@
+# --- !Ups
+
+CREATE TABLE Make (
+    id bigserial NOT NULL,
+    userId bigInt NOT NULL,
+    recipeId bigInt NOT NULL,
+    createdAt timestamp NOT NULL,
+    FOREIGN KEY (userId) REFERENCES GingrsnapUser(id) ON DELETE CASCADE,
+    FOREIGN KEY (recipeId) REFERENCES Recipe(id) ON DELETE CASCADE
+);
+
+# --- !Downs
+
+DROP TABLE Make CASCADE;
