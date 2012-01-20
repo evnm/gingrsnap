20cea6ad51a85d7a73f8b53ba7a9844463ebe7e7
Evan Meagher <evan.meagher@gmail.com>
2012-01-19 18:21:40 -0800

    Bootstrap task only does work in development, currently.

--- app/bootstrap.scala
+++ app/bootstrap.scala
@@ -7,46 +7,48 @@ import play.test._
 
 @OnApplicationStart class Bootstrap extends Job {
   override def doJob {
-    // Import initialization data if the database is empty.
-    if (GingrsnapUser.count().single() == 0) {
-      Logger.info("Bootstrap task: Importing initialization data into database")
-      Yaml[List[Any]]("init-data.yml") foreach {
-        _ match {
-          case u:  GingrsnapUser => GingrsnapUser.create(u)
-          case p:  Account => Account.create(p)
-          case r:  Recipe => Recipe.create(r)
-          case i:  Ingredient => Ingredient.create(i)
+    if (play.configuration("application.mode") == "dev") {
+      // Import initialization data if the database is empty.
+      if (GingrsnapUser.count().single() == 0) {
+        Logger.info("Bootstrap task: Importing initialization data into database")
+        Yaml[List[Any]]("init-data.yml") foreach {
+          _ match {
+            case u:  GingrsnapUser => GingrsnapUser.create(u)
+            case p:  Account => Account.create(p)
+            case r:  Recipe => Recipe.create(r)
+            case i:  Ingredient => Ingredient.create(i)
+          }
+        }
+
+        // SnakeYAML can't handle Option[Timestamp]s, so hand-publish each recipe.
+        val timestamp = new Timestamp(System.currentTimeMillis())
+        Recipe.find().list() foreach { recipe =>
+          Recipe.update(recipe.copy(publishedAt = Some(timestamp)))
         }
       }
 
-      // SnakeYAML can't handle Option[Timestamp]s, so hand-publish each recipe.
-      val timestamp = new Timestamp(System.currentTimeMillis())
-      Recipe.find().list() foreach { recipe =>
-        Recipe.update(recipe.copy(publishedAt = Some(timestamp)))
-      }
-    }
-
-    // Backfill events.
-    if (Event.count().single() == 0) {
-      Logger.info("Bootstrap task: Backfilling events")
-      Recipe.find().list() foreach { recipe =>
-        if (recipe.publishedAt.isDefined) {
-          if (recipe.parentRecipe.isDefined) {
-            Event.create(
-              Event(
-                NotAssigned,
-                EventType.RecipeFork.id,
-                subjectId = recipe.authorId,
-                objectId = recipe.id(),
-                createdAt = recipe.publishedAt.get))
-          } else {
-            Event.create(
-              Event(
-                NotAssigned,
-                EventType.RecipeCreation.id,
-                subjectId = recipe.authorId,
-                objectId = recipe.id(),
-                createdAt = recipe.createdAt))
+      // Backfill events.
+      if (Event.count().single() == 0) {
+        Logger.info("Bootstrap task: Backfilling events")
+        Recipe.find().list() foreach { recipe =>
+          if (recipe.publishedAt.isDefined) {
+            if (recipe.parentRecipe.isDefined) {
+              Event.create(
+                Event(
+                  NotAssigned,
+                  EventType.RecipeFork.id,
+                  subjectId = recipe.authorId,
+                  objectId = recipe.id(),
+                  createdAt = recipe.publishedAt.get))
+            } else {
+              Event.create(
+                Event(
+                  NotAssigned,
+                  EventType.RecipeCreation.id,
+                  subjectId = recipe.authorId,
+                  objectId = recipe.id(),
+                  createdAt = recipe.createdAt))
+            }
           }
         }
       }
