2ec381d16555bc5adcf00b97ffc30e52a8cf2138
Evan Meagher <evan.meagher@gmail.com>
2012-01-29 21:42:28 -0800

    Add feature and tip tables to drop tables script.

--- scripts/drop-tables.sh
+++ scripts/drop-tables.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-psql -d postgres --command "drop table play_evolutions, ingredient, recipe, account, gingrsnapuser, image, recipeimage, gingrsnapuserimage, make, event, passwordresetrequest cascade;"
+psql -d postgres --command "drop table play_evolutions, ingredient, recipe, account, gingrsnapuser, image, recipeimage, gingrsnapuserimage, make, event, passwordresetrequest, feature, tip cascade;"
