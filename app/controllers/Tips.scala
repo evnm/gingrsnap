906c72463aa7e354f57b700a9bad3c43e26ad509
Evan Meagher <evan.meagher@gmail.com>
2012-02-05 14:44:34 -0800

    Various style fixes
    * Restrict width of fb like element.
    * Shorten tip-success flash message.
    * Use Open Sans as body typeface.

--- app/controllers/Tips.scala
+++ app/controllers/Tips.scala
@@ -28,7 +28,7 @@ object Tips extends BaseController with Secure {
             } else {
               Tip.create(Tip(userId, recipeId, tipBody)).toOptionLoggingError match {
                 case Some(newTip) => {
-                  flash.success("Your tip has been left on this recipe")
+                  flash.success("Your tip has been left")
                 }
                 case None => {
                   Logger.error(
